f582a5afcd2e3b1da590a15c716273813d4593a1
hwidjajaja <hwidjaja2024@gmail.com>
2023-07-10 16:15:45 -0400

    Create blinkingtest.cc

---
+++ blinkingtest.cc
@@ -0,0 +1,15 @@
+int ledPin = 8;
+int delayTime - 500;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPIN, HIGH);
+    delay(delayTime);
+    digitalWrite(ledPIN, LOW);
+    delay(delayTime);
+}
